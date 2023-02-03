95b37d70c6173c78e5b9c8e897e7bf0c99749136
Eric1545 <100596011+Eric1545@users.noreply.github.com>
2023-02-03 15:29:17 +0100

    Update Database.java

--- src/main/java/com/enchere/utils/Database.java
+++ src/main/java/com/enchere/utils/Database.java
@@ -8,20 +8,13 @@ import java.sql.SQLException;
 
 public class Database {
 
-    private static Connection connection;/*
+    private static Connection connection;
     private static final String host = "containers-us-west-102.railway.app";
 
     private static final String database = "railway";
     private static final int port = 6347;
     private static final String user = "postgres";
     private static final String pass = "muWcYv3UjOvmQyqrcSWu";
-    private static String url = "jdbc:postgresql://%s:%d/%s";*/
-    private static final String host = "localhost";
-
-    private static final String database = "enchere";
-    private static final int port = 5432;
-    private static final String user = "postgres";
-    private static final String pass = "eric1545";
     private static String url = "jdbc:postgresql://%s:%d/%s";
 
     public Database()
