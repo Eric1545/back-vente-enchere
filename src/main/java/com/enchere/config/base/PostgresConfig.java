4071bbc00aca0524a6b57c34c840832fb66f1efb
Eric1545 <100596011+Eric1545@users.noreply.github.com>
2023-02-03 15:28:05 +0100

    Update PostgresConfig.java

--- src/main/java/com/enchere/config/base/PostgresConfig.java
+++ src/main/java/com/enchere/config/base/PostgresConfig.java
@@ -36,20 +36,13 @@ public class PostgresConfig {
 
     @Primary
     @Bean("postgresDatasource")
-    public DataSource dataSource() {/*
+    public DataSource dataSource() {
         DataSourceBuilder<?> dataSource = DataSourceBuilder.create();
         System.out.println(env.getProperty("org.postgresql.Driver"));
         dataSource.driverClassName("org.postgresql.Driver");
         dataSource.url("jdbc:postgresql://containers-us-west-102.railway.app:6347/railway");
         dataSource.username("postgres");
         dataSource.password("muWcYv3UjOvmQyqrcSWu");
-        return dataSource.build();*/
-        DataSourceBuilder<?> dataSource = DataSourceBuilder.create();
-        System.out.println(env.getProperty("org.postgresql.Driver"));
-        dataSource.driverClassName("org.postgresql.Driver");
-        dataSource.url("jdbc:postgresql://localhost:5432/enchere");
-        dataSource.username("postgres");
-        dataSource.password("eric1545");
         return dataSource.build();
     }
 
